48ebe8fa241d20288b986260a02a14c3d178b757
madheshkumar <76593246+madheshkumar@users.noreply.github.com>
2021-11-03 19:56:40 +0530

    Add files via upload

---
+++ Bill Counter/src/BillCounter.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class BillCounter extends NewJFrame{
+    public static void main(String[] args) {
+        BillCounter obj = new BillCounter();
+        obj.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        obj.setSize(900,650);
+        obj.setVisible(true);
+    }
+
+}
